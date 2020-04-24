8880246bc3571668f3638c07fe5910e7ec2726d2
o1lo01ol1o <tim.pierson@gmail.com>
2020-04-24 17:42:24 +0100

    implement test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,43 @@
-module Main (main) where
+module Main
+    ( main
+    )
+where
+
+import           Data.Maybe                     ( isJust
+                                                , isNothing
+                                                )
+import           Numeric.Financial
+import           Test.Tasty
+import           Test.Tasty.QuickCheck         as QC
 
 
 main :: IO ()
-main = putStrLn ("Test suite is not implemented" :: String)
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [properties]
+
+properties :: TestTree
+properties = testGroup "Property tests" [irrTerminates]
+
+irrTerminates :: TestTree
+irrTerminates = QC.testProperty
+    "That internal rate of return finds a solution "
+    irrTerminates'
+
+irrTerminates' :: [Double] -> Bool
+irrTerminates' cfs = isJust r || isNothing r
+    where r = internalRateOfReturn (fmap (fromRational . toRational) cfs)
+
+paymentPartsEqualToComposit :: TestTree
+paymentPartsEqualToComposit = QC.testProperty
+    "That compenent parts are always equal to the sum "
+    paymentPartsEqualToComposit'
+
+paymentPartsEqualToComposit'
+    :: Double -> Int -> Double -> Double -> Bool -> Int -> Bool
+paymentPartsEqualToComposit' rate nper pv fv payAtBeginning per =
+    pmt == (i + p)
+  where
+    (i, p) = paymentParts rate nper pv fv payAtBeginning per
+    pmt    = payment rate nper pv fv payAtBeginning
